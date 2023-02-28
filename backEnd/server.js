b44006139e1a6c787c687af458b0500917bda2ad
Huaira Nimrod <nimax29@gmail.com>
2023-02-28 03:41:53 -0700

    mongo db connection example

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const MongoClient = require('mongodb').MongoClient; // imports the mongoDB client
+const mongodb = require('./db/connect');//
+
 const professionalRoutes = require('./routes/professional');
-const PORT = 8080;
+const port = process.env.PORT || 8080;
 const app = express();
 
 app
@@ -13,8 +16,11 @@ app
   })
   .use('/professional', professionalRoutes); //
 
-
-
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+      console.log(err);
+    } else {
+      app.listen(port);
+      console.log(`Connected to DB and listening on ${port}`);
+    }
+  });
